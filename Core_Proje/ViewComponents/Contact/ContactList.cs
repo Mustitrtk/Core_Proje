ec6e08a02a6b28c16400b65b37ef466c71354235
Mustafa TORTUK <mustafat237@hotmail.com>
2024-05-13 13:46:10 +0300

    iletişim viewcomponent oluşturuldu

---
+++ Core_Proje/ViewComponents/Contact/ContactList.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Contact
+{
+    public class ContactList : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
---
+++ Core_Proje/ViewComponents/SendMessage/SendMessage.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.SendMessage
+{
+    public class SendMessage : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
