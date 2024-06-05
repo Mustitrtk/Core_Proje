caee6ba6609c499db7898bcf5dc18045fadb4655
Mustafa TORTUK <mustafat237@hotmail.com>
2024-06-05 16:41:07 +0300

    footer ve navbar partial oluşturuldu, yetenekler listelendi.

--- Core_Proje/Controllers/AdminController.cs
+++ Core_Proje/Controllers/AdminController.cs
@@ -4,7 +4,17 @@ namespace Core_Proje.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult PartialSidebar()
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
         {
             return PartialView();
         }
--- Core_Proje/Controllers/SkillController.cs
+++ Core_Proje/Controllers/SkillController.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
 {
     public class SkillController : Controller
     {
+        SkillManager _skillManager = new SkillManager(new EfSkillDal());
         public IActionResult Index()
         {
-            return View();
+            var values = _skillManager.GetAll();
+            return View(values);
         }
     }
 }
