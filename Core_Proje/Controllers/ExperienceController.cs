daa0b93631a7cd3cd3333de731f1e2b1f64ded34
Mustafa TORTUK <mustafat237@hotmail.com>
2024-06-24 17:52:26 +0300

    Deneyim ekleme ve silme tamamlandı.

---
+++ Core_Proje/Controllers/ExperienceController.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        ExperienceManager _experienceManager = new ExperienceManager(new EfExperienceDal());
+        public IActionResult Index()
+        {
+            var values = _experienceManager.GetAll();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddExperience() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddExperience(Experience e)
+        {
+            _experienceManager.TAdd(e);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteExperience(int id) 
+        {
+            _experienceManager.TDelete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
